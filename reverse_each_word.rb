a21e9316b59432d37d8e80e65e3508f26b59818a
Lee McCormick <lzm0010@gmail.com>
2020-01-24 23:36:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(str)
-  reverse_array = []
-  array = str.split(" ").collect{|word| 
-    new_word = word.reverse
-    reverse_array << new_word
-  }
-  reverse_array.join(' ')
+  # reverse_array = []
+  # array = str.split(" ").collect{|word| 
+  #   new_word = word.reverse
+  #   reverse_array << new_word
+  # }
+  # reverse_array.join(' ')
 end
